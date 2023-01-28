58ef4c00882fc89a989592130afedc2d09e916de
Victor Rodrigues <victor.rsantos@outlook.com>
2023-01-28 17:53:06 -0300

    Criando Microsservico de Payments - Parte 1

---
+++ DevFreela.Payments.API/Controllers/PaymentController.cs
@@ -0,0 +1,28 @@
+﻿using DevFreela.Payments.API.Models;
+using DevFreela.Payments.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DevFreela.Payments.API.Controllers
+{
+    [Route("api/payments")]
+    public class PaymentController : ControllerBase
+    {
+        private readonly IPaymentsService _paymentsService;
+
+        public PaymentController(IPaymentsService paymentsService)
+        {
+            _paymentsService = paymentsService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] PaymentInfoInputModel paymentInfoInputModel)
+        {
+            var result = await _paymentsService.Process(paymentInfoInputModel);
+
+            if (!result) { return BadRequest(); }
+
+            return NoContent();
+        }
+    }
+}
---
+++ DevFreela.Payments.API/Services/PaymentServices.cs
@@ -0,0 +1,13 @@
+﻿using DevFreela.Payments.API.Models;
+using System.Threading.Tasks;
+
+namespace DevFreela.Payments.API.Services
+{
+    public class PaymentServices : IPaymentsService
+    {
+        public Task<bool> Process(PaymentInfoInputModel paymentInfoInputModel)
+        {
+           return Task.FromResult(true);
+        }
+    }
+}
